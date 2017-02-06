afc415dd9777530ca98523069b0ace25a9fb5eb1
Fideo <FideoLJ@gmail.com>
2017-02-07 02:47:38 +0800

    Finish app without auth

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
+
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
