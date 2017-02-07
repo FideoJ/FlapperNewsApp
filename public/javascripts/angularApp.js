dda36d74aa6bd3546d6c409e8b9033b9610afcd3
Fideo <FideoLJ@gmail.com>
2017-02-07 18:34:47 +0800

    Finish User-Aware App

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -26,12 +26,34 @@ function($stateProvider, $urlRouterProvider) {
           return posts.get($stateParams.id);
         }]
       }
-    });
+    })
+
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if (auth.isLoggedIn()) {
+          $state.go('home');
+        }
+      }]
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if (auth.isLoggedIn()) {
+          $state.go('home');
+        }
+      }]
+    })
 
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', ['$http', function($http) {
+app.factory('posts', ['$http', 'auth', function($http, auth) {
   var o = {
     posts: []
   };
@@ -41,13 +63,16 @@ app.factory('posts', ['$http', function($http) {
     });
   };
   o.create = function(post) {
-    return $http.post('/posts', post).success(function(data){
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer ' + auth.getToken()}
+    }).success(function(data){
       o.posts.push(data);
     });
   };
   o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote')
-      .success(function(data) {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer ' + auth.getToken()}
+    }).success(function(data) {
         post.upvotes += 1;
       });
   };
@@ -57,11 +82,14 @@ app.factory('posts', ['$http', function($http) {
     });
   };
   o.addComment = function(id, comment) {
-    return $http.post('/posts/' + id + '/comments', comment);
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer ' + auth.getToken()}
+    });
   };
   o.upvoteComment = function(post, comment) {
-    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
-      .success(function(data){
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer ' + auth.getToken()}      
+    }).success(function(data){
         comment.upvotes += 1;
       });
   };
@@ -114,14 +142,17 @@ app.factory('auth', ['$http', '$window', function($http, $window) {
   auth.logOut = function() {
     $window.localStorage.removeItem('flapper-news-token');
   };
-  
+
   return auth;
 }]);
 
 app.controller('MainCtrl', [
-'$scope', 'posts',
-function($scope, posts) {
+'$scope',
+'posts',
+'auth',
+function($scope, posts, auth) {
   $scope.posts = posts.posts;
+  $scope.isLoggedIn = auth.isLoggedIn;
 
   $scope.incrementUpvotes = function(post) {
     posts.upvote(post);
@@ -142,8 +173,10 @@ app.controller('PostsCtrl', [
 '$scope',
 'posts',
 'post',
-function($scope, posts, post){
+'auth',
+function($scope, posts, post, auth){
   $scope.post = post;
+  $scope.isLoggedIn = auth.isLoggedIn;
   
   $scope.incrementUpvotes = function(comment) {
     posts.upvoteComment(post, comment);
@@ -159,4 +192,37 @@ function($scope, posts, post){
     });
     $scope.body = '';
   };
+}]);
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth) {
+  $scope.user = {};
+
+  $scope.register = function() {
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function() {
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}]);
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth) {
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
 }]);
